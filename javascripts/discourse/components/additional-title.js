4b46ad5430fe0dfbfd6149762507814239327974
gal-samuel-israel <webmaster@agency.co.il>
2024-01-28 19:11:49 +0200

    try setupComponent

--- javascripts/discourse/components/additional-title.js
+++ javascripts/discourse/components/additional-title.js
@@ -2,66 +2,12 @@ import Component from "@ember/component";
 import { inject as service } from "@ember/service";
 import User from "discourse/models/user";
 
-export default class additionalTitle extends Component{
-    @service site;
-
-    currentUser = User.current();
-    additionalTitle =  false;
-
-    constructor(){
-        super(...arguments);
-
-        var debugForAdmins = settings.enable_debug_for_admins;
-        var debugForAll = settings.enable_debug_for_all;
-        var currentUser = this.currentUser;
-        var userName = (args.model === undefined) ? args.user.username : currentUser.username;
-        var debug = (currentUser.admin && debugForAdmins) ? true : (!currentUser.admin && debugForAll) ? true : false;
-        if(debug){
-            console.log('debug on for card extender:');
-            console.log('Admins: ' + debugForAdmins);
-            console.log('All: ' + debugForAll);
-            console.log('currentUser:', currentUser);
-            console.log('args:', args);
-            console.log('component:', Component);
-            console.log('card userName:', userName);
-        }
-        /*
-        if(userName !== undefined){
-            jQuery.ajax({                
-                method : 'GET',
-                url : '/u/'+userName+'.json',
-                success: function(result){
-                    //console.log(result);
-                    var userGroups = result.user.groups;
-                    var userTitle = result.user.title;
-                    var isEmployee = false;
-
-                    if(userGroups?.length > 2){
-                        isEmployee = userGroups.some((item)=>{
-                            return item.name === "Algosec" || item.name === "staff" ;
-                        });         
-                    }
-
-                    var calcTitle = userTitle;
-                    if(isEmployee){
-                        calcTitle = (calcTitle===undefined || calcTitle==="" || calcTitle===null) ? 'AlgoSec Employee' : 'AlgoSec';
-                    }
-
-                    component.set("additionalTitle", calcTitle);
-
-                    if(debug){
-                        console.log('userGroups: ', userGroups);
-                        console.log('userTitle: ', userTitle);
-                        console.log('isEmployee: ', isEmployee);
-                        console.log('calcTitle: ', calcTitle);
-                    }
-                },
-            });
-        }*/
-
-    }
-    
-    willDestroy() {
-        this.additionalTitle = null;
-    }
-}
+export default {
+    setupComponent() {
+      this.reopen({
+        router: service(),
+        currentUser: User.current(),
+        additionalTitle: false,
+      });
+    },
+};
