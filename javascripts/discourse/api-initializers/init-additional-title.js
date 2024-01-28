4becace48606bf16b1a5e40484d03b4f5162db26
gal-samuel-israel <webmaster@agency.co.il>
2024-01-28 19:37:42 +0200

    render in 2 outlets

--- javascripts/discourse/api-initializers/init-additional-title.js
+++ javascripts/discourse/api-initializers/init-additional-title.js
@@ -3,7 +3,7 @@ import additionalTitle from "../components/additional-title";
 
 export default apiInitializer("1.6", (api) => {
     const outlet1 = 'user-card-post-names';
-    const outlet2 = 'user-post-names';
+     // NO NEED // const outlet2 = 'user-post-names';
     api.renderInOutlet(outlet1, additionalTitle);
-    api.renderInOutlet(outlet2, additionalTitle);
+    // NO NEED // api.renderInOutlet(outlet2, additionalTitle);
 });
