40fa80b0102110cfc6b4785d59cbb80b7ba0c97f
gal-samuel-israel <webmaster@agency.co.il>
2024-01-28 20:26:34 +0200

    try again with component folders

--- javascripts/discourse/api-initializers/init-additional-title.js
+++ javascripts/discourse/api-initializers/init-additional-title.js
@@ -2,8 +2,8 @@ import { apiInitializer } from "discourse/lib/api";
 import additionalTitle from "../components/user-post-names/additional-title";
 
 export default apiInitializer("1.6", (api) => {
-    const outlet1 = 'user-post-names'; //'user-card-post-names';
-     // NO NEED // const outlet2 = 'user-post-names';
+    const outlet1 = 'user-card-post-names';
+    const outlet2 = 'user-post-names';
     api.renderInOutlet(outlet1, additionalTitle);
-    // NO NEED // api.renderInOutlet(outlet2, additionalTitle);
+    api.renderInOutlet(outlet2, additionalTitle);
 });
