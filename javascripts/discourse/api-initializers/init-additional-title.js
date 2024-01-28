8439a8b6d5cbeec384af794b3141374007575565
gal-samuel-israel <webmaster@agency.co.il>
2024-01-28 19:33:44 +0200

    try again

--- javascripts/discourse/api-initializers/init-additional-title.js
+++ javascripts/discourse/api-initializers/init-additional-title.js
@@ -1,9 +1,9 @@
 import { apiInitializer } from "discourse/lib/api";
 import additionalTitle from "../components/additional-title";
 
-export default apiInitializer("0.8", (api) => {
-  const outlet1 = 'user-card-post-names';
-  const outlet2 = 'user-post-names';
-  api.renderInOutlet(outlet1, additionalTitle);
-  api.renderInOutlet(outlet2, additionalTitle);
+export default apiInitializer("1.6", (api) => {
+    const outlet1 = 'user-card-post-names';
+    const outlet2 = 'user-post-names';
+    api.renderInOutlet(outlet1, additionalTitle);
+    // api.renderInOutlet(outlet2, additionalTitle);
 });
--- javascripts/discourse/components/additional-title.js
+++ javascripts/discourse/components/additional-title.js
@@ -1,13 +1,71 @@
 import Component from "@ember/component";
 import { inject as service } from "@ember/service";
 import User from "discourse/models/user";
+export default Component.extend({
+    router: service(),      
+    siteSettings: service(),
+    init() {
+        this._super(...arguments);
+        console.log('additionalTitle');
 
-export default {
-    setupComponent() {
-      this.reopen({
-        router: service(),
-        currentUser: User.current(),
-        additionalTitle: false
-      });
-    }
-}
+        this.showOnlyToAdmins = settings?.enable_modal_only_for_admins; //from settings.yml
+        this.debugForAdmins = settings?.enable_debug_for_admins; //from settings.yml
+        this.debugFooter = this.debugForAdmins && settings?.enable_modal_footer_internal_debug; //from settings.yml
+        this.debug4All = settings?.enable_debug_for_all; //from settings.yml    
+
+        this.debugForUsers = settings?.enable_debug_for_user_ids; //from settings.yml
+        var debugForIDs = (this.debugForUsers) ? this.debugForUsers.split("|") : null;
+        
+        this.debug = false;
+        if(this.currentUser?.admin && this.debugForAdmins){ this.debug = true; }
+        if(debugForIDs && debugForIDs.includes(this.currentUser.id.toString())) { this.debug = true; }
+        if(this.debug4All){ this.debug = true; }
+
+        if(this.debug){
+            console.log('component init start:');      
+        }
+
+        if(!this.currentUser || (!this.currentUser?.admin && this.showOnlyToAdmins)){
+        if(this.debug){
+            console.log('destroy');
+        }
+        this.destroying = true;
+        this.destroy();
+        return false;
+        }
+
+    },
+
+    didInsertElement() {      
+        this._super(...arguments);
+    
+        if(this.destroying){return;}
+    
+        if(this.debug){ console.log('didInsertElement'); } 
+      },
+    
+      didRender(){
+        this._super(...arguments);
+    
+        if(this.destroying){return;}
+        
+        //visual effects should not be done here as this is run many times
+        if(this.debug){ console.log('didRender'); } 
+      },
+    
+      willRender() {
+        if(this.debug){ console.log('willRender'); }   
+      },
+    
+      willDestroyElement(element){
+        if(this.debug){ console.log('willDestroyElement:', element); }  
+        //remove eventlisters here
+        //element.removeEventListener("keydown", this.handleTabKeyStrokes, true);
+    
+        this._super(...arguments);
+      },
+    
+      didDestroyElement() {
+        //document.documentElement.classList.remove("class-of-element-to-remove-from-dom");
+      },
+});
