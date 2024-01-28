f80ac3963f3c83dcd2847497c788e92c4038c868
gal-samuel-israel <webmaster@agency.co.il>
2024-01-28 19:45:55 +0200

    try user-profile-names

--- javascripts/discourse/api-initializers/init-additional-title.js
+++ javascripts/discourse/api-initializers/init-additional-title.js
@@ -2,7 +2,7 @@ import { apiInitializer } from "discourse/lib/api";
 import additionalTitle from "../components/additional-title";
 
 export default apiInitializer("1.6", (api) => {
-    const outlet1 = 'user-card-post-names';
+    const outlet1 = 'user-profile-names'; //'user-card-post-names';
      // NO NEED // const outlet2 = 'user-post-names';
     api.renderInOutlet(outlet1, additionalTitle);
     // NO NEED // api.renderInOutlet(outlet2, additionalTitle);
